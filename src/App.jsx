1606782cd4d4436648a4cf9258f810cbffc8452b
dk10119 <tranduclong092@gmail.com>
2024-04-06 16:38:27 +0300

    style the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Counter from "./Counter"
 import Header from "./Header"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 function App() {
   return (
     <>
       <Header />
-      <p>Hello world!</p>
       <Counter />
     </>
   )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,15 @@
-export default function Header() {
+import logo from './assets/react.svg'
+
+function Header() {
   return (
-    <div>Header</div>
+    <div className='py-3 px-2' style={{ background: 'white' }}>
+        <img 
+            src={logo} alt="React logo"
+            style={{ height: '40px', verticalAlign: 'top' }}
+        />
+        <span className='h1 pt-4 m-2'>React Counter</span>
+    </div>
   )
 }
+
+export default Header
