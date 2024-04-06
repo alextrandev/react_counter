30233305c1fc99ab3dc09042bf8c0d89ff3ee6d7
dk10119 <tranduclong092@gmail.com>
2024-04-06 16:48:23 +0300

    add attack and defence buttons

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,18 @@
 function Counter() {
-  return (
-    <div>Counter</div>
-  )
+    const handleAttack = () => {
+        console.log('Attack');
+    }
+    const handleDefence = () => {
+        console.log('Defence');
+    }
+
+    return (
+        <div className="row text-white">
+            <h1>Counter</h1>
+            <button onClick={handleAttack} style={{width: '200px'}}>+1</button>
+            <button onClick={handleDefence} style={{width: '200px'}}>-1</button>
+        </div>
+    )
 }
 
 export default Counter
